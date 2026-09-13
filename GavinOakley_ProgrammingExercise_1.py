cf8058c3fad4e7ef4bdcf4858b3d290d6d7b3e64
gavin <gavinoakleyyy@gmail.com>
2026-09-13 18:31:26 -0400

    Changed name of accumulator variable TOTAL_TICKETS to TICKETS_AVAILABLE.

--- GavinOakley_ProgrammingExercise_1.py
+++ GavinOakley_ProgrammingExercise_1.py
@@ -1,12 +1,12 @@
 #First, set up some constant variables as defaults to be used for counting in both functions.
-TOTAL_TICKETS = 10
+TICKETS_AVAILABLE = 10
 MAX_TICKETS = 4
 BUYERS = 0
 
 #Define the function for buying tickets, taking input and printing relevant info.
 def buy_tickets():
     #Defining the constant variables as global allows them to be modified in function.
-    global TOTAL_TICKETS, BUYERS
+    global TICKETS_AVAILABLE, BUYERS
 
     while True:
         tickets_bought = int(input("Please enter the amount of tickets to purchase. (Max. 4) : "))
@@ -14,20 +14,20 @@ def buy_tickets():
         if tickets_bought > MAX_TICKETS:
             print("Error: Max of 4 tickets per customer. \n")
             continue
-        if tickets_bought > TOTAL_TICKETS:
+        if tickets_bought > TICKETS_AVAILABLE:
             print("Not enough tickets available for purchase. \n")
             continue
 
-        TOTAL_TICKETS -= tickets_bought
+        TICKETS_AVAILABLE -= tickets_bought
         BUYERS += 1
 
-        print(f"There are {TOTAL_TICKETS} tickets left. \n")
+        print(f"There are {TICKETS_AVAILABLE} tickets left. \n")
         return
 
 #Define the main function which runs the buy_tickets function as long as there are
 #more than 0 tickets.
 def main():
-    while TOTAL_TICKETS > 0:
+    while TICKETS_AVAILABLE > 0:
         buy_tickets()
 
     print(f"There are no more tickets remaining. \nTotal buyers: {BUYERS}.")
